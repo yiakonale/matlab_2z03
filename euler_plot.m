316add06eb701598cf2d11e74f7683b3df1f5e00
Ioannis Papaspyridis <98855524+yiakonale@users.noreply.github.com>
2023-08-31 20:14:36 +0300

    Update euler_plot.m

--- euler_plot.m
+++ euler_plot.m
@@ -2,6 +2,6 @@
 %error = abs(y-yExact);
 %plot(x,yExact,'r',x1,y1,'.b',x2,y2,'.g',x3,y3,'.c'); % plotting
 plot(x,g,'r',x,y,'b');
-title('Ioannis Papaspyridis, 400363946'); % figure's title,
+title(''); % figure's title,
 legend('surrounding temperature g(t)','temperature of bolt T(t)'); % legend
 xlabel('x'); ylabel('y'); % and axis labels
